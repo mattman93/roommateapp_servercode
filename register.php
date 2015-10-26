8113f96bc8ff20fdc19ddae2590fd3a9bae6cdf8
mattman93 <sanibelmatt7@aol.com>
2015-10-26 03:20:11 -0400

    register.php

--- register.php
+++ register.php
@@ -4,4 +4,5 @@
  $user_name = $_POST["user_name"];  
  $user_pass = $_POST["user_pass"];  
  $sql_query = "INSERT INTO user_info values('$name','$user_name','$user_pass');";  
+  mysqli_query($con,$sql_query); 
  ?>  
