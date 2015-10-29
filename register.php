7709c23092afb47852d55ee92ea3cd8a178bdfda
root <sanibelmatt7@aol.com>
2015-10-29 17:50:25 -0400

    reg

--- register.php
+++ register.php
@@ -3,6 +3,8 @@
  $name = $_POST["user"];  
  $user_name = $_POST["user_name"];  
  $user_pass = $_POST["user_pass"];  
- $sql_query = "INSERT INTO user_info VALUES('$name','$user_name','$user_pass')";  
-  mysqli_query($con,$sql_query); 
+ $sql_query = "INSERT INTO user_info(name,user_name,user_pass) VALUES('$name','$user_name','$user_pass');";  
+ if($con->query($sql_query) === TRUE){
+	echo "Registration Success...";
+	} 
  ?>  
