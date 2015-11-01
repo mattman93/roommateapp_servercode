671f3f934de20f1259e1659f0cb57bce05209c37
root <sanibelmatt7@aol.com>
2015-11-01 16:48:06 -0500

    updated

--- get_user.php
+++ get_user.php
@@ -1,16 +1,20 @@
 <?php  
- require "init.php";  
- $name = $_POST["user"];  
+ require "init.php";    
  $email = $_POST["email"];  
  $user_pass = $_POST["password"];  
-
+//echo $email;
+//echo $user_pass;
  $sql_query = "SELECT * FROM Users WHERE email = '$email' AND password = '$user_pass';";  
- if ($result=mysqli_query($con,$sql_query))
-  {
- 	if($row=mysqli_fetch_row($result))
-    {
-    echo $row[0]."-".$row[1]."-".$row[2]."-".$row[3]."-".$row[4]);
-    }
+ $result=mysqli_query($con,$sql_query);
+ if(mysqli_num_rows($result) > 0) {
+ 	 $row=mysqli_fetch_assoc($result);
+	$fname = $row["first_name"];
+	$lname = $row["last_name"];
+	$uid = $row["id"];
+	$phonenum = $row['phone_number'];
+	$email = $row['email'];
+    
+ echo $uid."-".$fname."-".$lname."-".$email."-".$phonenum;
   //mysqli_free_result($result);
 }  
 
--- register.php
+++ register.php
@@ -1,10 +1,13 @@
 <?php  
  require "init.php";  
- $name = $_POST["user"];  
- $user_name = $_POST["user_name"];  
- $user_pass = $_POST["user_pass"];  
- $sql_query = "INSERT INTO user_info(name,user_name,user_pass) VALUES('$name','$user_name','$user_pass');";  
+ $name = $_POST["email"];  
+ $user_name = $_POST["fname"];  
+ $user_pass = $_POST["lname"];  
+ $sql_query = "INSERT INTO Users(email,first_name,last_name) VALUES('$name','$user_name','$user_pass');";  
  if($con->query($sql_query) === TRUE){
 	echo "Registration Success...";
-	} 
+	}
+echo $name;
+echo $user_name;
+echo $user_pass; 
  ?>  
