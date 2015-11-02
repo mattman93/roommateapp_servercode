2151836843b4f195ce96e07849250b3758fdb364
root <sanibelmatt7@aol.com>
2015-11-02 08:26:59 -0500

    edit_chore.php

--- edit_chore.php
+++ edit_chore.php
@@ -1,14 +1,14 @@
 <?php  
  require "init.php";  
 
- $title = $_POST["title"];  
- $desc = $_POST["desc"];  
- $requestUser = $_POST["requestUser"];  
- $assignedUser = $_POST["assignedUser"];  
- $groupid = $_POST['groupid'];
- $choreid = $_POST['choreid'];
+$title = $_POST["title"];
+$desc = $_POST["desc"];
+$requestUser = $_POST["requestUser"];
+$assignedUser = $_POST["assignedUser"];
+$choreid = $_POST["choreid"];  
+ 
+ $sql_query = "UPDATE Chores SET name = '$title', `desc` = '$desc', assigned_to = '$assignedUser', posted_by = '$requestUser' WHERE id = '$choreid';";
   
- $sql_query = "UPDATE Chores SET title = '$title', `desc` = '$desc', assigned_to = '$assignedUser', posted_by = '$requestUser' WHERE id = '$choreid');";  
  if($con->query($sql_query) === TRUE){
 	echo "Chore edited";
 	} else {
