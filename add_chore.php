bde811c2e3ca79974cf2593b2703320fd49ad13e
mattman93 <sanibelmatt7@aol.com>
2015-11-01 19:38:30 -0500

    add_chore.php

---
+++ add_chore.php
@@ -0,0 +1,16 @@
+<?php  
+ require "init.php";  
+
+ $title = $_POST["title"];  
+ $desc = $_POST["desc"];  
+ $requestUser = $_POST["requestUser"];  
+ $assignedUser = $_POST["assignedUser"];  
+ $groupid = $_POST['groupid'];
+  
+ $sql_query = "INSERT INTO Chores(title,`desc`,assigned_to,posted_by, groupid) VALUES('$title','$desc',' $assignedUser','$requestUser','$groupid');";  
+ if($con->query($sql_query) === TRUE){
+	echo "Chore added";
+	} else {
+		echo "Error adding chore";
+	}
+ ?> 
