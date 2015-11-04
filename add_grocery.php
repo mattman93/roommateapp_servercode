33787a7068c66d7c0d9cb17425625ac7e463900c
mattman93 <sanibelmatt7@aol.com>
2015-11-03 21:01:04 -0500

    add_grocery

---
+++ add_grocery.php
@@ -0,0 +1,20 @@
+<?php  
+ require "init.php";  
+
+ $item_name = $_POST["item_name"];  
+ $quantity = $_POST["quantity"];  
+ $dr = $_POST["dr"];  
+ $dp = $_POST["dp"];  
+ $ip = $_POST['ip']; 
+ $cst = $_POST["cst"];  
+ $req_user = $_POST["requestUser"];  
+ $purc_user = $_POST['purchaseUser'];
+ $groupid = $_POST['groupid'];
+  
+ $sql_query = "INSERT INTO Grocery_item(item_name, quantity, dateRequested, datePurchased, isPurchased, cost, requestUser, purchaseUser, groupid) VALUES('$item_name', '$quantity', '$dr', '$dp','$ip','$cst','$req_user','$purc_user') WHERE groupid = '$groupid';";  
+ if($con->query($sql_query) === TRUE){
+	echo "Grocery added";
+	} else {
+		echo "Error adding Grocery";
+	}
+ ?> 
