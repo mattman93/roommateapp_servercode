9e746a019cb8ea621ef2322cc7cc6e208bb9ffcd
root <sanibelmatt7@aol.com>
2015-11-03 20:09:03 -0500

    update get_grocery_list.php

--- get_grocery_list.php
+++ get_grocery_list.php
@@ -4,12 +4,12 @@
  $sql_query = "SELECT * FROM Grocery_item WHERE groupid = '$groupid' ORDER BY(id) DESC;";  
  $result=mysqli_query($con,$sql_query);
  if(mysqli_num_rows($result) > 0) {
-$chores = array();
+$Groceries = array();
   while($row = $result->fetch_assoc()){
         $Groceries[] = $row;
     }
 
-	echo json_encode($chores);    
+	echo json_encode($Groceries);    
 //print_r($chores);
 } 
 ?>
