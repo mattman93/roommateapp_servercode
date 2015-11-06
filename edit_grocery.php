25a11b95fcba23008a6b9038ad157543a198d0b9
root <sanibelmatt7@aol.com>
2015-11-06 10:41:28 -0500

    fixes

--- edit_grocery.php
+++ edit_grocery.php
@@ -8,7 +8,7 @@
  $cst = $_POST["cst"];  
  $req_user = $_POST["requestUser"];  
  $purc_user = $_POST['purchaseUser'];
- $grc_id = $_GET["grcid"];
+ $grc_id = $_POST["grcid"];
  $sql_query = "UPDATE Grocery_item SET item_name = '$item_name', quantity = '$quantity', dateRequested = '$dr', isPurchased = '$ip', cost = '$cst', requestUser = '$req_user', purchaseUser = '$purc_user' WHERE id = '$grc_id';";  
  if($con->query($sql_query) === TRUE){
 	echo "Grocery edited";
