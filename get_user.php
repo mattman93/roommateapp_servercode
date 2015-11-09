54be03e41b582aa53a849f97d0a6b72a7ef0dbf5
mattman93 <sanibelmatt7@aol.com>
2015-11-09 14:04:51 -0500

    fixed get_user.php

--- get_user.php
+++ get_user.php
@@ -1,22 +1,16 @@
 <?php  
  require "init.php";    
- $email = $_POST["email"];  
- $user_pass = $_POST["password"];  
-//echo $email;
-//echo $user_pass;
- $sql_query = "SELECT * FROM Users WHERE email = '$email' AND password = '$user_pass';";  
+ $dev_id = $_POST["deviceid"];  
+ $sql_query = "SELECT * FROM Users WHERE DeviceId = '$dev_id' LIMIT 1";  
  $result=mysqli_query($con,$sql_query);
  if(mysqli_num_rows($result) > 0) {
- 	 $row=mysqli_fetch_assoc($result);
-	$fname = $row["first_name"];
-	$lname = $row["last_name"];
-	$uid = $row["id"];
-	$phonenum = $row['phone_number'];
-	$email = $row['email'];
-    
- echo $uid."-".$fname."-".$lname."-".$email."-".$phonenum;
-  //mysqli_free_result($result);
-}  
+ 	 
+	$user_data = array();
+  		while($row = $result->fetch_assoc()){
+     	   $user_data[] = $row;
+   		 }
+		echo json_encode($user_data);    
+	}  
 
 
 
