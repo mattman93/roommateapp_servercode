688c54f287fa7d66fcedebaab300757c6dab4816
mattman93 <sanibelmatt7@aol.com>
2015-11-09 13:43:39 -0500

    get_user_list.php

---
+++ get_user_list.php
@@ -0,0 +1,13 @@
+<?php  
+ require "init.php";    
+ $groupid = $_POST["groupid"];  
+ $sql_query = "SELECT * FROM Users WHERE groupid = '$groupid' ORDER BY(id) DESC;";  
+ $result=mysqli_query($con,$sql_query);
+ if(mysqli_num_rows($result) > 0) {
+$users = array();
+  while($row = $result->fetch_assoc()){
+        $users[] = $row;
+    }
+	echo json_encode($users);    
+} 
+?>
