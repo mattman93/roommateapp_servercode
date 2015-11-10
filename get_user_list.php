4d21933bbd5cdf295485653dfd339a9b3701478d
root <sanibelmatt7@aol.com>
2015-11-10 13:00:17 -0500

    fixes

--- get_user_list.php
+++ get_user_list.php
@@ -1,7 +1,7 @@
 <?php  
  require "init.php";    
  $groupid = $_POST["groupid"];  
- $sql_query = "SELECT * FROM Users WHERE groupid = '$groupid' ORDER BY(id) DESC;";  
+ $sql_query = "SELECT * FROM Users WHERE group_id = '$groupid' ORDER BY(id) DESC;";  
  $result=mysqli_query($con,$sql_query);
  if(mysqli_num_rows($result) > 0) {
 $users = array();
